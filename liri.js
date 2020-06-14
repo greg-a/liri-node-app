8727f942db6e88cbdddcddfc258df42c1c8ae766
Greg Allebach <greg.allebach@gmail.com>
2020-06-14 18:04:10 -0400

    now logs data returned from each call in a text file

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 var axios = require("axios");
 var moment = require("moment");
+var fs = require("fs");
 
 var userCommand = process.argv[2];
 var userSearch = process.argv[3];
@@ -11,6 +12,9 @@ if (process.argv.length > 4) {
 }
 
 if (userCommand === "movie-this") {
+    if (userSearch === undefined) {
+        userSearch = "Mr. Nobody"
+    };
     axios.get("http://www.omdbapi.com/?t=" + userSearch + "&apikey=trilogy").then(
         function (response) {
             //    * Title of the movie.
@@ -29,16 +33,30 @@ if (userCommand === "movie-this") {
             console.log("Plot: " + response.data.Plot);
             //    * Actors in the movie.
             console.log("Actors: " + response.data.Actors);
+
+            var text = "Title: " + response.data.Title + ", " + "Release year: " + response.data.Year + ", " + "IMDB Rating: " + response.data.imdbRating + ", " + "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + ", " + "Produced in: " + response.data.Country + ", " + "Language: " + response.data.Language + ", " + "Plot: " + response.data.Plot + ", " + "Actors: " + response.data.Actors + "|";
+
+            fs.appendFile("log.txt", text, function(err) {
+                console.log("------");
+                console.log("Successfully logged data!")
+            });
         })
 };
 if (userCommand === "concert-this") {
     axios.get("https://rest.bandsintown.com/artists/" + userSearch + "/events?app_id=codingbootcamp").then(
         function (response) {
             for (var i = 0; i < response.data.length; i++) {
-            console.log("\n---------\n")
+            console.log("------");
             console.log("Venue: " + response.data[i].venue.name);
             console.log("Location: " + response.data[i].venue.location);
             console.log("Date: " + moment(response.data[i].datetime).format('LL'));
+
+            var text = "Band: " + response.data[i].artist.name + ", " + "Venue: " + response.data[i].venue.name + ", " + "Location: " + response.data[i].venue.location + ", " + "Date: " + moment(response.data[i].datetime).format('LL') + "|";
+
+            fs.appendFile("log.txt", text, function(err) {
+                console.log("------");
+                console.log("Successfully logged data!")
+            });
             }
         })
 };
